5a28b20b1ee4e43168f7344ddebf353681028a9c
Lidia <lidiabazhenova@gmail.com>
2018-06-27 17:08:53 +0300

    assertEquals

--- CollectionsProject/src/test/java/com/lidiabazhenova/task8/Task8Test.java
+++ CollectionsProject/src/test/java/com/lidiabazhenova/task8/Task8Test.java
@@ -1,5 +1,6 @@
 package com.lidiabazhenova.task8;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,20 +11,19 @@ public class Task8Test {
     @Test
     public void task8Test() throws IOException {
 
-        List<Map<String,List<String>>> list1 = new ArrayList<>();
+        List<Map<String, List<String>>> list1 = new ArrayList<>();
         Map<String, List<String>> map1 = new HashMap<>();
         Map<String, List<String>> map2 = new HashMap<>();
 
         map1.put("mykey1", Arrays.asList("one", "two", "three"));
         map1.put("mykey2", Arrays.asList("four", "five", "six"));
-        System.out.println(map1);
         list1.add(map1);
 
         map2.put("mykey1", Arrays.asList("seven", "eight", "nine"));
         map2.put("mykey2", Arrays.asList("ten", "eleven", "twelve"));
         list1.add(map2);
 
-        List<Map<String,List<String>>> list2 = new ArrayList<>();
+        List<Map<String, List<String>>> list2 = new ArrayList<>();
         Map<String, List<String>> map3 = new HashMap<>();
         Map<String, List<String>> map4 = new HashMap<>();
 
@@ -31,16 +31,14 @@ public class Task8Test {
         map3.put("mykey2", Arrays.asList("four", "five", "six"));
         list2.add(map3);
 
-        map4.put("mykey1", Arrays.asList("zero", "eight", "nine"));
-        map4.put("mykey2", Arrays.asList("ten", "eleven", "twelve"));
-
+        map4.put("mykey1", Arrays.asList("nine", "eight", "seven"));
+        map4.put("mykey2", Arrays.asList("ten", "twelve", "eleven"));
         list2.add(map4);
 
-
         Comparator<String> myComparator = (s1, s2) -> {
-            if (s1==null && s2==null) return 0;
-            if (s1==null) return  1;
-            if (s2==null) return -1;
+            if (s1 == null && s2 == null) return 0;
+            if (s1 == null) return 1;
+            if (s2 == null) return -1;
             return s1.compareTo(s2);
         };
 
@@ -56,8 +54,6 @@ public class Task8Test {
             }
         });
 
-        System.out.println(list1.equals(list2));
-        System.out.println(list1);
-        System.out.println(list2);
+        Assert.assertEquals(list1, list2);
     }
 }
