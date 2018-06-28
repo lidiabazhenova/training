41a9abc13ff2b13746bdd5fb8229e944e3b7f00d
Lidia <lidiabazhenova@gmail.com>
2018-06-28 10:59:04 +0300

    task2: stream().map()

--- CollectionsProject/src/test/java/com/lidiabazhenova/task2/Task2Test.java
+++ CollectionsProject/src/test/java/com/lidiabazhenova/task2/Task2Test.java
@@ -8,24 +8,26 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Результат первого задания отфильтровать, результатом будет такая же коллекция, только с колонками first name и age
  */
 public class Task2Test {
     private static final String USERS_CSV_FILE_PATH = "src/test/resources/users.csv";
+    private static final String key1 = "first name";
+    private static final String key2 = "age";
 
     @Test
     public void task2Test() throws IOException {
         List<Map<String, String>> people = CSVReaderUtil.loadAllPersonsFromFile(USERS_CSV_FILE_PATH);
-        List<Map<String, String>> newPeople = new ArrayList<>();
 
-        people.forEach((person) -> {
+        final List<Map<String, String>> newPeople = people.stream().map((person) -> {
             Map<String, String> newPerson = new HashMap<>();
-            newPerson.put("first name", person.get("first name"));
-            newPerson.put("age", person.get("age"));
-            newPeople.add(newPerson);
-        });
+            newPerson.put(key1, person.get(key1));
+            newPerson.put(key2, person.get(key2));
+            return newPerson;
+        }).collect(Collectors.toList());
 
         System.out.println(newPeople);
     }
