b620aa7d5b7db94644a9c76f905ec3531cd13311
Lidia <lidiabazhenova@gmail.com>
2018-06-28 15:50:41 +0300

    task13

---
+++ CollectionsProject/src/test/java/com/lidiabazhenova/task13/Task13Test.java
@@ -0,0 +1,52 @@
+package com.lidiabazhenova.task13;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Отсортировать внешний и внутренний листы в List<Map<String, List<String>>>
+ */
+public class Task13Test {
+    private static final String key = "one";
+
+    @Test
+    public void task13Test() throws IOException {
+
+        List<Map<String, List<String>>> list = new LinkedList<>();
+        Map<String, List<String>> map1 = new HashMap<>();
+        Map<String, List<String>> map2 = new HashMap<>();
+        List<String> list1 = new LinkedList<>(Arrays.asList("one", "two", "three"));
+        List<String> list2 = new LinkedList<>(Arrays.asList("four", "five", "six"));
+        List<String> list3 = new LinkedList<>(Arrays.asList("seven", "eight", "nine"));
+        List<String> list4 = new LinkedList<>(Arrays.asList("one", "eight", "nine"));
+
+        map1.put("mykey1", list1);
+        map1.put("mykey2", list2);
+        list.add(map1);
+
+        map2.put("mykey3", list3);
+        map2.put("mykey4", list4);
+        list.add(map2);
+
+        list.forEach((map) -> {
+
+            for (List<String> listString : map.values()) {
+                for (Iterator<String> iterator = listString.iterator(); iterator.hasNext(); ) {
+                    String value = iterator.next();
+                    if (value.equals("one")) {
+                        iterator.remove();
+                    }
+                }
+            }
+        });
+
+        //TODO Assertion
+        list.forEach((map) -> {
+            System.out.println(map.values());
+
+        });
+    }
+}
