2e267fe2186f314c0d9d8ded111fcaae697e2451
Lidia <lidiabazhenova@gmail.com>
2018-06-28 10:50:59 +0300

    task10

---
+++ CollectionsProject/src/test/java/com/lidiabazhenova/task10/Task10Test.java
@@ -0,0 +1,29 @@
+package com.lidiabazhenova.task10;
+
+import com.lidiabazhenova.common.CSVReaderUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * Получить из коллекции List<Map<String, String>> коллекцию, где будут извлеченные значения из мапы по заданному клучу
+ */
+public class Task10Test {
+    private static final String USERS_CSV_FILE_PATH = "src/test/resources/users.csv";
+    private static final String key = "last name";
+
+    @Test
+    public void task10Test() throws IOException {
+        List<Map<String, String>> people = CSVReaderUtil.loadAllPersonsFromFile(USERS_CSV_FILE_PATH);
+
+        final List<String> valuesByKey = people.stream().map((person) -> person.get(key)).collect(Collectors.toList());
+
+        System.out.println(valuesByKey);
+    }
+}
