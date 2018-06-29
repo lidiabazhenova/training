e70d729d444e61a2a63c59af72024a51836782b2
Lidia <lidiabazhenova@gmail.com>
2018-06-29 12:07:10 +0300

    task7: change collection for map(Long, Man), for treemap

---
+++ CollectionsProject/src/test/java/com/lidiabazhenova/task16/Task16Test.java
@@ -0,0 +1,17 @@
+package com.lidiabazhenova.task16;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Task16Test {
+
+    @Test
+    public void task16Test() {
+        List<String> list = new LinkedList<>(Arrays.asList("one", "two", "three"));
+        String csv = String.join(",", list);
+        System.out.println(csv);
+    }
+}
--- CollectionsProject/src/test/java/com/lidiabazhenova/task7/Task7Test.java
+++ CollectionsProject/src/test/java/com/lidiabazhenova/task7/Task7Test.java
@@ -10,29 +10,34 @@ import java.util.stream.Collectors;
 
 /**
  * Создать объект Man, с полями id, name, age, sex. Создать список этих объектов. Преобразовать в коллекцию, которая будет содержать строковое представление со значениями id и name.
- 8. Есть две коллекции List<Map<String, List<String>>> с разным содержанием. Сравните их
  */
 public class Task7Test {
 
+    private Man Ivan = new Man(11110000, "Ivan", 22, "male");
+    private Man Alex = new Man(11110001, "Alex", 34, "male");
+    private Man Valeria = new Man(11110002, "Valeria", 29, "female");
+
+
     @Test
     public void task7Test() throws IOException {
-        List<Man> manList = new ArrayList<>();
-        manList.add(new Man(11110000, "Ivan", 22, "male"));
-        manList.add(new Man(11110001, "Alex", 34, "male"));
-        manList.add(new Man(11110002, "Valeria", 29, "female"));
+        Map<Long, Man> manList = new TreeMap<>();
+        manList.put(Ivan.getId(), Ivan);
+        manList.put(Alex.getId(), Alex);
+        manList.put(Valeria.getId(), Valeria);
 
-        final List<Map<String, String>> newManList = manList.stream().map((man) -> {
+        manList.forEach((id, man) -> System.out.println(man.getId() + " = " + man.getName()));
+
+        final List<Map<String, String>> newManList = manList.entrySet().stream().map((man) -> {
             Map<String, String> newPerson = new HashMap<>();
-            newPerson.put("id", String.valueOf(man.getId()));
-            newPerson.put("name", man.getName());
+            newPerson.put(String.valueOf(man.getKey()), man.getValue().getName());
             return newPerson;
         }).collect(Collectors.toList());
 
-        Assert.assertEquals("11110000", newManList.get(0).get("id"));
-        Assert.assertEquals("Ivan", newManList.get(0).get("name"));
-        Assert.assertEquals("11110001", newManList.get(1).get("id"));
-        Assert.assertEquals("Alex", newManList.get(1).get("name"));
-        Assert.assertEquals("11110002", newManList.get(2).get("id"));
-        Assert.assertEquals("Valeria", newManList.get(2).get("name"));
+        System.out.println(newManList);
+
+        Assert.assertEquals("Ivan", newManList.get(0).get("11110000"));
+        Assert.assertEquals("Alex", newManList.get(1).get("11110001"));
+        Assert.assertEquals("Valeria", newManList.get(2).get("11110002"));
+
     }
 }
