6e00a58ec2cc896e6f3b20a7e7191284bcbc5a05
Lidia <lidiabazhenova@gmail.com>
2018-06-29 12:57:45 +0300

    task15

--- CollectionsProject/src/test/java/com/lidiabazhenova/task15/Task15Test.java
+++ CollectionsProject/src/test/java/com/lidiabazhenova/task15/Task15Test.java
@@ -2,10 +2,26 @@ package com.lidiabazhenova.task15;
 
 import org.junit.Test;
 
+import java.util.*;
+
 public class Task15Test {
 
     @Test
-    void tast15Test(){
+    public void test15Test(){
+        List<List<String>> list = new ArrayList<>();
 
+        list.add(Arrays.asList("10", "11", "12"));
+        list.add(Arrays.asList("10", "11", "12"));
+        list.add(Arrays.asList("10", "11", "12"));
+        list.add(Arrays.asList("10", "11", "10"));
+        list.add(Arrays.asList("1", "2", "3"));
+        list.add(Arrays.asList("1", "2", "3"));
+
+        Set<List<String>> hs = new HashSet<>();
+        hs.addAll(list);
+        list.clear();
+        list.addAll(hs);
+
+        System.out.println(hs);
     }
 }
