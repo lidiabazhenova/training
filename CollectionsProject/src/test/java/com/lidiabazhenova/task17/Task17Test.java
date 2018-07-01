a31ba086cf73b8b2e4751c209822326875281a42
Lidia <lidiabazhenova@gmail.com>
2018-07-01 14:39:08 +0300

    task17 add, get, remove

--- CollectionsProject/src/test/java/com/lidiabazhenova/task17/Task17Test.java
+++ CollectionsProject/src/test/java/com/lidiabazhenova/task17/Task17Test.java
@@ -6,41 +6,152 @@ import java.util.*;
 
 public class Task17Test {
 
+    private static final int ELCOUNT = 5000;
+
     @Test
-    public void task17Test() {
-        ArrayList<Integer> arrayList = new ArrayList();
-
-        for (int i = 0; i < 1000000; i++) { // Заполнение коллекции последовательностью чисел от 0 до 1000000
-            arrayList.add(i);
-        }
-
-        Collections.shuffle(arrayList); // организация произвольного порядка
-
-        long arrayListTime = checkTime(arrayList);
-        System.out.println(arrayListTime);
+    public void task17InTheMidleListTest() {
+        List<Integer> arrayList = new ArrayList();
+        System.out.print("ArrayList: ");
+        createList(arrayList);
 
         LinkedList<Integer> linkedList = new LinkedList<>();
+        System.out.print("LinkedList: ");
+        createList(linkedList);
 
-        for (int i = 0; i < 1000000; i++) { // Заполнение коллекции последовательностью чисел от 0 до 1000000
-            linkedList.add(i);
-        }
+        System.out.println("\tadd in the middle of the list:");
+        System.out.print("ArrayList: ");
+        System.out.print(estimateAddInTheMiddle(arrayList));
+        System.out.print("\tLinkedList: ");
+        System.out.println(estimateAddInTheMiddle(linkedList));
 
-        Collections.shuffle(linkedList); // организация произвольного порядка
+        System.out.println("\tremove in the middle of the list:");
+        System.out.print("ArrayList: ");
+        System.out.print(estimateRemoveInTheMiddle(arrayList));
+        System.out.print("\tLinkedList: ");
+        System.out.println(estimateRemoveInTheMiddle(linkedList));
 
-        long linkedListTime = checkTime(linkedList);
-        System.out.println(linkedListTime);
+        System.out.println("\tget in the middle of the list:");
+        System.out.print("ArrayList: ");
+        System.out.print(estimateGetInTheMiddle(arrayList));
+        System.out.print("\tLinkedList: ");
+        System.out.println(estimateGetInTheMiddle(linkedList));
 
+        System.out.println("ArrayList size = " + arrayList.size());
+        System.out.println("LinkedList size = " + linkedList.size());
+        System.out.println("-//-//-//-//-//-//--//-//-//-//-//-//-//-//-//-");
     }
 
-    private long checkTime(List list) {
-        Date startMap = new Date();
-        for (int i = 0; i < list.size() / 50; i++) {
-            //операция .add .insert. remove. get .set с начала середины, и конца списка
-            //k - кол-во операций
+    @Test
+    public void task17AtTheEndListTest() {
+        List<Integer> arrayList = new ArrayList();
+        createList(arrayList);
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        createList(linkedList);
+
+        System.out.println("\tadd at the end of the list:");
+        System.out.print("ArrayList: ");
+        System.out.print(estimateAddToEnd(arrayList));
+        System.out.print("\tLinkedList: ");
+        System.out.println(estimateAddToEnd(linkedList));
+
+        System.out.println("\tremove at the end of the list:");
+        System.out.print("ArrayList: ");
+        System.out.print(estimateRemoveFromEnd(arrayList));
+        System.out.print("\tLinkedList: ");
+        System.out.println(estimateRemoveFromEnd(linkedList));
+
+        System.out.println("\tget at the end of the list:");
+        System.out.print("ArrayList: ");
+        System.out.print(estimateGetFromEnd(arrayList));
+        System.out.print("\tLinkedList: ");
+        System.out.println(estimateGetFromEnd(linkedList));
+
+        System.out.println("ArrayList size = " + arrayList.size());
+        System.out.println("LinkedList size = " + linkedList.size());
+        System.out.println("-//-//-//-//-//-//--//-//-//-//-//-//-//-//-//-");
+    }
+
+    private List<Integer> createList(final List<Integer> list) {
+        final long start = System.currentTimeMillis();
+        for (int i = 1; i <= 1000000; i++) {
+            list.add(i);
+        }
+
+        final long finish = System.currentTimeMillis();
+        System.out.print("add 1000000 elements: ");
+        System.out.println(finish-start);
+
+        return list;
+    }
+
+    private long estimateGetInTheMiddle(final List<Integer> list) {
+        final long start = System.currentTimeMillis();
+        final int startIndex = list.size() / 2;
+
+        for (int i = 0; i < ELCOUNT; i++) {
+            list.get(startIndex + i);
+        }
+        final long finish = System.currentTimeMillis();
+
+        return finish - start;
+    }
+
+    private long estimateAddInTheMiddle(final List<Integer> list) {
+        final long startList = System.currentTimeMillis();
+        final int startIndex = list.size() / 2;
+
+        for (int i = 0; i < ELCOUNT; i++) {
+            list.add(startIndex, i);
+        }
+        final long finishList = System.currentTimeMillis();
+
+        return finishList - startList;
+    }
+
+    private long estimateRemoveInTheMiddle(final List<Integer> list) {
+        final long startList = System.currentTimeMillis();
+        final int startIndex = list.size() / 2;
+
+        for (int i = 0; i < ELCOUNT; i++) {
+            list.remove(startIndex + i);
+        }
+        final long finishList = System.currentTimeMillis();
+
+        return finishList - startList;
+    }
+
+    private long estimateGetFromEnd(final List<Integer> list) {
+        final long startList = System.currentTimeMillis();
+        final int startIndex = list.size() - 1;
+
+        for (int i = startIndex; i >= list.size() - 10; i--) {
             list.get(i);
         }
-        Date finishMap = new Date();
-        return finishMap.getTime() - startMap.getTime();
+        final long finishList = System.currentTimeMillis();
+
+        return finishList - startList;
     }
 
+    private long estimateAddToEnd(final List<Integer> list) {
+        final long startList = System.currentTimeMillis();
+
+        for (int i = 0; i < ELCOUNT; i++) {
+            list.add(i);
+        }
+        final long finishList = System.currentTimeMillis();
+
+        return finishList - startList;
+    }
+
+    private long estimateRemoveFromEnd(final List<Integer> list) {
+        final long startList = System.currentTimeMillis();
+
+        for (int i = 0; i < ELCOUNT; i++) {
+            list.remove(list.size() - 1);
+        }
+        final long finishList = System.currentTimeMillis();
+
+        return finishList - startList;
+    }
 }
